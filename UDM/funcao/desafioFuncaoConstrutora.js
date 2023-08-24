c300bf1056257bcc80245d5d388cff31f673f2d1
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-08-24 16:45:00 -0300

    24/08/23

---
+++ UDM/funcao/desafioFuncaoConstrutora.js
@@ -0,0 +1,10 @@
+function pessoa(nome){
+    this.nome = nome
+
+    this.falar = function(){    
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new pessoa('Lucas')
+p1.falar()
