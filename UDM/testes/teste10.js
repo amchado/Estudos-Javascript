edf289907b84f22c5567ab88b0e5a78d82d7069e
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-11-01 16:35:52 -0300

    01/11/23

---
+++ UDM/testes/teste10.js
@@ -0,0 +1,12 @@
+function divisao(numero){
+    if(numero % 3 == 0 ){
+        return true
+    }else{
+        return false
+    }
+}   
+
+console.log(divisao(3))
+console.log(divisao(92))
+console.log(divisao(14))
+console.log(divisao(27))
