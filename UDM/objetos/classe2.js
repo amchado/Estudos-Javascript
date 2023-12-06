e9ec4bf86ed9070d5ce414dc1ce215aa2f01172d
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-12-06 15:40:25 -0300

    06/12/23

---
+++ UDM/objetos/classe2.js
@@ -0,0 +1,25 @@
+class Avo{
+    constructor(sobrenome){
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'professor'){
+        super(sobrenome)
+        this.profissao = profissao
+    }
+}
+
+class Filho extends Pai {
+    constructor(){
+        super('Silva')
+    }
+}
+
+const filho = new Filho
+const avo = new Avo
+const pai = new Pai
+console.log(filho)
+console.log(pai)
+console.log(avo)
