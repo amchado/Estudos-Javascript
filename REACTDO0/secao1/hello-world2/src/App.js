8a3251e0b05ea5ba2debc045c82cbc9cf61e3233
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-12-19 17:38:24 -0300

    19/12/23

---
+++ REACTDO0/secao1/hello-world2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World React</h1>
+      <h2>Emmet funcionou</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+
