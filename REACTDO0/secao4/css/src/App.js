924b0631f7470acc425593885b80d4b8c4c0d103
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-09 19:48:34 -0300

    09/01/24

--- REACTDO0/secao4/css/src/App.js
+++ REACTDO0/secao4/css/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyComponents from './components/MyComponents'
+import {useState} from "react"
+import Title from './components/Title';
 
 function App() {
+  const n = 15
+
+  const [nome] = useState("Arthur")
+
+  const redTitle = false
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Css Global */}
+      <h1>React com CSS</h1>
+
+      {/* Css de componente */}
+      <MyComponents></MyComponents>
+      <p>este paragrafo é do app.js</p>
+
+      {/* Css inline */}
+      <p style={{color:"red", backgroundColor:"lightgreen", padding:"10px"}}>este elemento foi modificado com css inline</p>
+
+      {/* Css inline dinamico */}
+      <h2 style={n < 10 ? ({
+        color:"purple"
+      }) : ({color:"lightpink"})}>Css dinâmico</h2>
+
+      <h2 style={n > 10 ? ({
+        color:"purple"
+      }) : ({color:"lightpink"})}>Css dinâmico</h2>
+
+      <h2 style={nome === 'Arthur' ? ({
+        color:"green", backgroundColor:"darkred"
+      }) : null }>Css dinâmico</h2>
+
+      {/* classe dinamica*/}
+      <h2 className={redTitle ? "red-title" : "title" }>Este titulo vai ter classe dinâmica</h2>
+
+      {/* CSS modules */}
+     <Title></Title>
     </div>
+    
   );
 }
 
---
+++ REACTDO0/secao4/css/src/components/MyComponents.js
@@ -0,0 +1,15 @@
+import "./MyComponents.css"
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h1>
+            Css de componente
+        </h1>
+        <p>Este é o paragrafo do componente </p>
+        <p className="my-comp-paragraph">Este tb é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponents
