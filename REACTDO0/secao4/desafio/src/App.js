c548413e3d4277a8a22a7fda721441db88411edc
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-17 20:17:33 -0300

    17/01/24

--- REACTDO0/secao4/desafio/src/App.js
+++ REACTDO0/secao4/desafio/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Cars from './componentes/Cars'
+
+
+const myCars = [{modelo:'Onix', km:12000, color:'Vermelho'},{
+  modelo:'Captur',km:5500,color:'Preto'},{
+    modelo:'Celta',km:1000,color:'Prata'
+  }]
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Showroom de carros</h1>
+      <div className='car-conteiner'>
+       {myCars.map((car)=>(
+        <Cars car={car}/>
+       ))}
+      </div>
     </div>
   );
 }
