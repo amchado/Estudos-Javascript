59068831e6b7c1be8714c2512debc6184bd895ac
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-16 21:00:01 -0300

    16/01/24

--- REACTDO0/secao4/challengecss/src/components/Car.js
+++ REACTDO0/secao4/challengecss/src/components/Car.js
@@ -2,7 +2,7 @@ import styles from './Car.module.css'
 
 const Car = (car) => {
   return (
-    <div>
+    <div className={styles.card}>
       <h1>{car.modelo}</h1>
       <p>KM:{car.km}</p>
       <p>Cor:{car.color}</p>
