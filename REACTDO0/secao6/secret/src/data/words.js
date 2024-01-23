503195cd8c014ee64b486b8041fa0fe72fbdce78
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-23 17:02:11 -0300

    23/01/24

---
+++ REACTDO0/secao6/secret/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList= {
+    carro:["Motor", "Porta", "Capô", "Pneu", "Lanterna"  ],
+    fruta:["uva", "Laranja", "Banana", "Maçã", "Goiaba"],
+    corpo:["Mão", "Cerebro", "Pescoço", "Perna", "Barriga"],
+    computador:["Mouse", "Teclado", "Monitor", "Gabinete", "Webcam"],
+    alimento:["Arroz", "Feijão", "ovo", "Leite", "Macarrão"],
+    objeto:["Faca", "Colher", "Vassoura", "Travisseiro", "Toalha"]
+};
