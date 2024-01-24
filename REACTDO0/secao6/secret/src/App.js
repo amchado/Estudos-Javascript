05d05f3348b8d5ea2f3760315a44b279aca0ea98
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-24 13:36:39 -0300

    24/01/24

--- REACTDO0/secao6/secret/src/App.js
+++ REACTDO0/secao6/secret/src/App.js
@@ -33,23 +33,23 @@ function App() {
   const [guesses, setGuesses] = useState(guessesNunber)
   const [score, setScore] = useState(0)
   
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words)
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
 
-    console.log(category)
+   //console.log(category)
 
     //pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)]
 
-    console.log(word);
+    //console.log(word);
 
     return{word, category}
-  }
+  },[words]);
 
   //starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
     // clear all letters
     clearLetterStates();
 
@@ -62,8 +62,8 @@ function App() {
 
     wordLetters = wordLetters.map((l)=> l.toLowerCase())
 
-    console.log(word, category)
-    console.log(wordLetters)
+    //console.log(word, category)
+    //console.log(wordLetters)
 
     //fill states
     setPickedWord(word)
@@ -71,7 +71,7 @@ function App() {
     setLetters(wordLetters)
 
     setGameStage(stages[1].name)
-  };
+  }, [pickWordAndCategory]);
 
   //processo the letter input
   const verifyLetter = (letter) => {
@@ -122,7 +122,7 @@ function App() {
   //check win condition
   useEffect(()=>{
 
-    const uniqueLetters = [... new Set(letters)]
+    const uniqueLetters = [...new Set(letters)]
 
     //win condition
     if(guessedLetters.length === uniqueLetters.length){
