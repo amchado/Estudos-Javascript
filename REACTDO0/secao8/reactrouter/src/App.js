68b607acef44caaefbf9963228f29860cbec48b1
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-01-29 22:13:51 -0300

    29/01/24

---
+++ REACTDO0/secao8/reactrouter/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+//1 - cofing react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+//pages
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
