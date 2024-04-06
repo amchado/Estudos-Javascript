138898ce6cc389f28af6296c112a3fbcf23e3b8d
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-04-06 12:23:48 -0300

    06/04/24 tarefas adicionadas apenas no backend

--- REACTDO0/secao18/todo-react/src/App.tsx
+++ REACTDO0/secao18/todo-react/src/App.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 //components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,14 +12,17 @@ import TaskForm from "./components/TaskForm";
 import {ITask} from "./interfaces/Task"
 
 
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div>
       <Header/>
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar tarefa"/>
+          <TaskForm btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Suas tarefas:</h2>
--- REACTDO0/secao18/todo-react/src/components/TaskForm.tsx
+++ REACTDO0/secao18/todo-react/src/components/TaskForm.tsx
@@ -8,16 +8,29 @@ import {ITask} from '../interfaces/Task'
 
 type Props = {
   btnText:string
+  taskList: ITask[]
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
 }
 
-const TaskForm = ({btnText}: Props) => {
+const TaskForm = ({btnText, taskList, setTaskList}: Props) => {
 
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>("")
   const [difficulty, setDifficulty]=useState<number>(0)
 
-  const addTaskHandler = () => {
+  const addTaskHandler = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
+    const id = Math.floor(Math.random() * 1000)
+
+    const newTask: ITask = {id, title, difficulty} 
+
+    setTaskList!([...taskList,newTask])
+
+    setTitle("")
+    setDifficulty(0)
+
+    console.log(taskList)
   }
 
   const handleChange = (e:ChangeEvent<HTMLInputElement>) => { 
@@ -26,8 +39,7 @@ const TaskForm = ({btnText}: Props) => {
     }else{
       setDifficulty(parseInt(e.target.value))
     }
-    console.log(title)
-    console.log(difficulty)
+    
   }
 
   return (
@@ -35,7 +47,7 @@ const TaskForm = ({btnText}: Props) => {
       <div  className={styles.input_container}>
         <label htmlFor="
         title">Título:</label>
-        <input type="text" name='title' placeholder='Título da tarefa' onChange={handleChange}/>
+        <input type="text" name='title' placeholder='Título da tarefa' onChange={handleChange} value={title}/>
       </div>
 
       <div className={styles.input_container}>
@@ -45,7 +57,7 @@ const TaskForm = ({btnText}: Props) => {
           name="difficulty"
           placeholder="Dificuldade da tarefa (1 a 5)"
           value={difficulty}
-          onChange={handleChange}
+          onChange={handleChange} min={0} max={5}
         />
       </div>
       <input type="submit" value={btnText} onChange={handleChange}/>
